04fa8a444b7736ee758189724363e7d3a2828a6b
Jared Smith <smithjar@oregonstate.edu>
2020-10-20 16:35:49 -0700

    New batch of plots. What I've learned from this run: the rises that we're looking for seem to happen only around 0.2 <= aggDistance < 0.4.  Will focus on this range when testing from now on.
    For this run I included aggChance = 1 as a control of sorts.  As expected, there are no rises for this value.  However, it also appears that aggChance >= 0.3 will also not give the desired behavior.  Will take a close look at this range in future runs.

--- genPlot.py
+++ genPlot.py
@@ -3,18 +3,21 @@ import numpy as np
 import math
 import os
 
-def main(penalty, aggDistance, aggCount, minChance):
-    for b in np.linspace(0.01, aggDistance, 10):
+def main(aggWeight, aggDistance, aggCount, aggChance):
+    #after some testing, it seems like the desired effect does
+    #not happen until aggDistance > 0.20
+    #so that is why this is our lower bound
+    for b in np.linspace(0.20, aggDistance, 6):
         folder = "distance=%s/"%(b)
         path = os.path.join('./plots/',folder)
         os.mkdir(path)
-        for a in np.linspace(1/2*penalty, penalty, 3):
-            for c in np.linspace(1,25,5):
-                for d in np.linspace(1/2*minChance,minChance,3):
+        for a in np.linspace(1/10*aggWeight, aggWeight, 5):
+            for c in np.linspace(1,aggCount,5):
+                for d in np.linspace(1/10*aggChance,aggChance,5):
                     print(a,b,c,d)
                     plt=pen.main(a,b,math.floor(c),d)
-                    plt.savefig(os.path.join(path,"penalty=%s,count=%s,chance=%s.png"%(a,c,d)))
+                    plt.savefig(os.path.join(path,"aggWeight=%s,count=%s,chance=%s.png"%(a,c,d)))
                     plt.close()
 
 if __name__ == '__main__':
-    main(0.5,0.25,25,0.5)
+    main(1,0.70,25,1)
